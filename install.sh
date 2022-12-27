72eb6fd92511606c521d4d9176eac86508d1d318
khoramsim-personal-laptop <khoramism@khoramism.com>
2022-12-27 23:05:18 +0330

    adding files

---
+++ install.sh
@@ -0,0 +1,21 @@
+
+
+
+
+apt install -y apt-utils
+sleep 1
+apt-get remove docker docker-engine docker.io containerd runc
+sleep 1 
+apt-get update 
+sleep 1 
+apt-get install -y ca-certificates curl gnupg lsb-release git
+sleep 1 
+apt-get update
+sleep 1  
+curl -fsSL https://get.docker.com -o get-docker.sh
+sleep 1 
+sh get-docker.sh
+groupadd docker
+sermod -aG docker $USER
+newgrp docker
+
---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import uuid
+import json
+from pathlib import Path
+
+# LOAD CONFIG FILE
+
+path = Path(__file__).parent.joinpath('v2ray/config/config.json')
+file = open(str(path), 'r', encoding='utf-8')
+config = json.load(file)
+
+# INPUT: UPSTREAM UUID
+
+defaultUUID = config['inbounds'][0]['settings']['clients'][0]['id']
+#if defaultUUID == '<UPSTREAM-UUID>':
+#    message = "Upstream UUID: (Leave empty to generate a random one)\n"
+#else:
+#    message = f"Upstream UUID: (Leave empty to use `{defaultUUID}`)\n"
+
+#upstreamUUID = input(message)
+#if upstreamUUID == '':
+#    if defaultUUID == '<UPSTREAM-UUID>':
+#        
+#    else:
+#        upstreamUUID = defaultUUID
+upstreamUUID = str(uuid.uuid4())
+config['inbounds'][0]['settings']['clients'][0]['id'] = upstreamUUID
+
+# SAVE CONFIG FILE
+
+content = json.dumps(config, indent=2)
+open(str(path), 'w', encoding='utf-8').write(content)
+
+# PRINT OUT RESULT
+
+print('Upstream UUID:')
+print(upstreamUUID)
+print('\nDone!')
