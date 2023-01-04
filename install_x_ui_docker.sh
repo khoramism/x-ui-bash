6c2ffd9866321f06a948313d63323c6672c440df
khoramsim-personal-laptop <khoramism@khoramism.com>
2023-01-04 12:01:47 +0330

    fix small

--- install_x_ui_docker.sh
+++ install_x_ui_docker.sh
@@ -3,5 +3,5 @@ REAL_IP=$(curl -s -m 5 ifconfig.io)
 mkdir x-ui
 mv ./docker-compose.yml x-ui 
 cd x-ui
-docker-compose up -d
+docker compose up -d
 echo "you can visit it at http://$REAL_IP:54321 /n Remember to change the port and password of panel"
